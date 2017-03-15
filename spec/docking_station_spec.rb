9ce4af7f386d604d1e40075a26a220b4d6b6185f
Simba14 <seanblundell@gmail.com>
2017-03-15 16:39:44 +0000

    Bikes array added

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -17,12 +17,12 @@ describe DockingStation do
   describe '#dock' do
     it 'docking bike' do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq [bike]
     end
 
     it "raises an error when the docking station is full" do
       bike = Bike.new
-      subject.dock(bike)
+      20.times { subject.dock Bike.new }
       expect {subject.dock(bike) }.to raise_error 'Docking station is full'
     end
   end
