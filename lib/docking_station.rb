00f8da05cc8eb8d205257b15f151e5cadee573a6
Simba14 <seanblundell@gmail.com>
2017-03-15 16:50:22 +0000

    Refactored based on SRP

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,13 +6,25 @@ class DockingStation
   end
 
   def release_bike
-      fail 'No bikes available' unless !@bikes.empty?
+      fail 'No bikes available' if empty?
       @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station is full' if @bikes.length >= 20
+    fail 'Docking station is full' if full?
     @bikes << bike
   end
+
+  private
+
+  def full?
+    @bikes.length >= 20 ? true : false
+  end
+
+  def empty?
+    @bikes.empty? ? true : false
+  end
+
+
 
 end
