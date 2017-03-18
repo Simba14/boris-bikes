1a635340b192758803138651a84b9e6ad6b98a2b
Simba14 <seanblundell@gmail.com>
2017-03-18 19:30:14 +0000

    Mocking behaviour using doubles and method stubs

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,15 +2,17 @@ require 'docking_station'
 
 describe DockingStation do
 
+  let(:bike) { Bike.new }
+
   it 'returns a bike that is working' do
-    bike = Bike.new
+    allow(bike).to receive(:working?).and_return(true)
     subject.dock(bike)
     expect(bike).to be_working
   end
 
   describe 'initialization' do
       subject { DockingStation.new }
-      let(:bike) { Bike.new }
+
 
       it 'has variable capacity' do
         docking_station = DockingStation.new(50)
@@ -29,27 +31,26 @@ describe DockingStation do
     it { is_expected.to respond_to(:dock).with(1).argument }
 
     it "docks working bikes" do
-      bike = Bike.new
       expect(subject.dock(bike)).to eq [bike]
     end
 
     it "docks broken bikes" do
-      bike = Bike.new
+      allow(bike).to receive(:report_broken).and_return(true)
       bike.report_broken
       expect(subject.dock(bike)).to eq [bike]
     end
-  
+
 
 
     it "raises an error when docking station is at full capacity" do
       docking_station = DockingStation.new(50)
-      50.times { docking_station.dock(Bike.new) }
-      expect { docking_station.dock(Bike.new) }.to raise_error "Docking station full"
+      50.times { docking_station.dock(bike) }
+      expect { docking_station.dock(bike) }.to raise_error "Docking station full"
     end
 
     it "raises an error using default capacity" do
-      subject.capacity.times { subject.dock(Bike.new) }
-      expect { subject.dock(Bike.new) }.to raise_error "Docking station full"
+      subject.capacity.times { subject.dock(bike) }
+      expect { subject.dock(bike) }.to raise_error "Docking station full"
     end
   end
 
@@ -61,22 +62,21 @@ describe DockingStation do
 
   describe '#release_bike' do
     it 'releases a bike' do
-      bike = Bike.new
+      allow(bike).to receive(:broken?)
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
 
     it "releases working bikes" do
-      bike = Bike.new
+      allow(bike).to receive_messages(:broken? => false, :working? => true)
       subject.dock(bike)
-      subject.release_bike
-      expect(bike).to be_working
+      released_bike = subject.release_bike
+      expect(released_bike).to be_working
     end
 
     it "does not release broken bikes" do
-      bike = Bike.new
+      allow(bike).to receive_messages(:broken? => true, :report_broken => true)
       subject.dock(bike)
-      bike.report_broken
       expect{ subject.release_bike }.to raise_error "This bike is broken"
     end
 
